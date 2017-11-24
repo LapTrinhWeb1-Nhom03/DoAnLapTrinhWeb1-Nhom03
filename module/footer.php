6263043e4dff4773094d89531fc36190e954d84f
Bùi Thanh Thiện <thanhthientt2797@gmail.com>
2017-11-24 23:54:49 +0700

    footer

---
+++ module/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<body>
+<footer id="footer"><!--Footer-->
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>Liên hệ</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+								<li><a href="https://www.youtube.com" target="_blank">Youtube</a></li>
+								<li><a href="https://twitter.com/?lang=vi" target="_blank">Twitter</a></li>
+								<li><a href="https://www.instagram.com/?hl=vi" target="_blank">Instagram</a></li>
+								<li><a href="https://www.tumblr.com" target="_blank">Tumblr</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>Loại xe</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="mercedes.php">Mercedes</a></li>
+								<li><a href="audi.php">Audi</a></li>
+								<li><a href="bmw.php">BMW</a></li>
+								<li><a href="lexus.php">Lexus</a></li>
+								<li><a href="bentley.php">Bently</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Showroom </h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a>22 Cộng Hòa, Tân Bình</a></li>
+								<li><a>702 Lũy Bán Bích, Q11</a></li>
+								<li><a>285 CMT8, Q10</a></li>
+								<li><a>28 Võ Văn Tần, Q3</a></li>
+								<li><a>81 Hàm Nghi, Q1</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>Auto Salon</h2>
+							<div class="searchform">
+								<p>Địa chỉ: 227 Nguyễn Văn Cừ, <br />phường 4, quận 5, Tp.HCM </br> 
+									ĐT: 0169 410 9469
+								</br> Chuyên cung cấp các loại xe: Audi, BMW, Lexus, Mercedess, Bentley
+							</br>Ý kiến phản hồi: autosalon@gmail.com
+						</p>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2017 Auto Salon Inc. All rights reserved.</p>
+					<p class="pull-right">Designed by <span>Group 03</span></p>
+				</div>
+			</div>
+		</div>
+	</footer><!--/Footer-->
+</body>
+</html>
