c43f82ef25fe55463881373fffb3e07a5a743b28
Bùi Thanh Thiện <thanhthientt2797@gmail.com>
2017-11-24 08:15:21 +0700

    module

---
+++ module/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<body>
+<header id="header"><!--header-->
+		
+		<div class="header-middle"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.php"><img src="images/home/logo.png" width="140" height="40" alt="" /></a>
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i>Giỏ hàng</a></li>
+								<li><a href="dang-nhap.php"><i class="fa fa-user"></i> Đăng nhập</a></li>
+								<li><a href="dang-ky.php"><i class="fa fa-lock"></i>Đăng ký</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--header_top-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php" class="active">Trang chủ</a></li>
+								<li class="dropdown"><a href="#">Thương hiệu<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.html">Mercedes</a></li>
+										<li><a href="product-details.html">Audi</a></li> 
+										<li><a href="checkout.html">BMW</a></li> 
+										<li><a href="cart.html">Lexus</a></li> 
+										<li><a href="login.html">Bentley</a></li> 
+                                    </ul>
+                                </li> 
+								<li class="dropdown"><a href="#">Loại xe<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="blog.html">Sedan</a></li>
+										<li><a href="blog-single.html">SUV</a></li>
+										<li><a href="blog-single.html">Convertible</a></li>
+										<li><a href="blog-single.html">Van</a></li>
+                                    </ul>
+                                </li> 
+							</ul>
+						</div>
+					</div>
+						<div class="container">
+							<div class="row">
+      						     <div id="custom-search-input">
+                    		        <div class="input-group col-md-2">
+                       		         <input type="text" class="  search-query form-control" placeholder="Search" />
+                        		        <span class="input-group-btn">
+                                		    <button class="btn btn-danger" type="button">
+                          		              <span class=" glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
+
+
+</body>
+</html>
