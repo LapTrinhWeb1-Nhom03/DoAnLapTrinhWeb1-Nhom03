08321976dcc034bd3b334dae9ace61b44bbfa049
Trương Quốc Thịnh <quocthinhlk@gmail.com>
2017-12-06 00:19:12 -0800

    sửa

--- dang-nhap.php
+++ dang-nhap.php
@@ -49,7 +49,6 @@
     include("module/footer.php");
    ?>
 
-
  <script src="js/jquery.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/jquery.scrollUp.min.js"></script>
